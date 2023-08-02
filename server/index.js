7f5abfbb8baad8f604d1f256e2b66e245c154849
cameronmcc <camsthrowaway89@gmail.com>
2023-08-01 18:28:23 -0700

    Got graphQL test routes coming back successfully

--- server/index.js
+++ server/index.js
@@ -1,9 +1,9 @@
 const express = require('express');
 require('dotenv').config();
+const graphql = require('graphql');
 const { graphqlHTTP } = require('express-graphql');
 const schema = require('./schema/schema');
 const port = process.env.PORT || 5000;
-
 const app = express();
 
 app.use('/graphql', graphqlHTTP({
--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,8 +1,8 @@
 const { projects, clients} = require('../sampleData.js');
 
-const graphql = require('graphql');
 
-const { GraphQLObjectType, GraphQLID, GraphQLString } = require('graphql');
+
+const { GraphQLObjectType, GraphQLID, GraphQLString, GraphQLSchema, GraphQLList } = require('graphql');
 
 //Client Type
 const ClientType = new GraphQLObjectType({
@@ -10,17 +10,30 @@ const ClientType = new GraphQLObjectType({
     fields: () => ({
         id: { type: GraphQLID},
         name: { type: GraphQLString},
-        email: { type: GraphQLString},
-        phone: { type: GraphQLString},
-        
+        description: { type: GraphQLString},
+        status: { type: GraphQLString},
     })
 })
 
 const RootQuery = new GraphQLObjectType({
     name: 'RootQueryType',
     fields: {
+        clients: {
+            type: new GraphQLList(ClientType),
+            resolve(parent, args){
+                return clients
+        }},
         client: {
             type: ClientType,
+            args: { id: { type: GraphQLID } },
+            resolve(parent, args){
+                //code to get data from db
+                return clients.find(client => client.id == args.id);
+            }
         }
     }
+});
+
+module.exports = new GraphQLSchema({
+    query: RootQuery    
 })
