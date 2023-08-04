985dc6f1e659367bd5d68c43b0209ab6a8157358
cameronmcc <camsthrowaway89@gmail.com>
2023-08-04 13:26:20 -0700

    Revisiting MUI is starting to feel REAL GOOD.. cleanup with appbar stuff

--- src/components/Appbar.jsx
+++ src/components/Appbar.jsx
@@ -6,20 +6,22 @@ import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
 import IconButton from "@mui/material/IconButton";
 import MenuIcon from "@mui/icons-material/Menu";
+import AssignmentTurnedInIcon from "@mui/icons-material/AssignmentTurnedIn";
 
 const Appbar = () => {
   return (
-    <AppBar className="app-bar" sx={{}}>
-      <Toolbar>
-        <Button color="inherit">Login</Button>
+    <AppBar className="app-bar" sx={{ pl: 0 }}>
+      <Toolbar
+        sx={{
+          display: "flex",
+          flexDirection: "row",
+          justifyContent: "spaceAround",
+          alignItems: "center",
+        }}
+      >
+        <AssignmentTurnedInIcon sx={{ fontSize: 48 }} />
         <Typography variant="h3">Project Manager Pro</Typography>
-        <IconButton
-          size="large"
-          edge="end"
-          color="inherit"
-          aria-label="menu"
-          sx={{ mr: 2 }}
-        >
+        <IconButton size="large" edge="end" color="inherit" aria-label="menu">
           <MenuIcon />
         </IconButton>
       </Toolbar>
